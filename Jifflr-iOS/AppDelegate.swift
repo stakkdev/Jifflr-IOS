e896d6dc343dc400e2aca5f752ef8f5617b3858c
James Shaw <james.shaw@thedistance.co.uk>
2018-01-22 14:31:24 +0000

    JIF-15 - Added environment configurations to project.

--- Jifflr-iOS/AppDelegate.swift
+++ Jifflr-iOS/AppDelegate.swift
@@ -21,18 +21,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
 
+        print("Running environment: \(Constants.currentEnvironment.rawValue)")
+
         //FirebaseApp.configure()
         Fabric.with([Crashlytics.self])
-
-        // AdMob
-        GADMobileAds.configure(withApplicationID: "ca-app-pub-6220129917785469~1943942885")
-
-        let configuration = ParseClientConfiguration {
-            $0.applicationId = "ad-m8"
-            $0.server = "https://adm8.thecore.thedistance.co.uk/parse"
-            $0.isLocalDatastoreEnabled = true
-        }
-        Parse.initialize(with: configuration)
+        self.configParse(in: application, with: launchOptions)
+        self.configAdmob()
 
         self.window = UIWindow(frame: UIScreen.main.bounds)
         let navController = UINavigationController()
@@ -52,25 +46,54 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+
     }
     
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+
     }
     
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+
     }
 }
 
+// Parse Config
+extension AppDelegate {
+    func configParse(in application: UIApplication, with launchOptions: [UIApplicationLaunchOptionsKey: Any]?) {
+
+        let configuration = ParseClientConfiguration {
+            $0.applicationId = "ad-m8"
+            $0.server = Constants.currentEnvironment.appURL
+            $0.clientKey = "vmN358nahRWnB3nTeyzxgyX7q28Lz4fN"
+            $0.isLocalDatastoreEnabled = true
+            $0.networkRetryAttempts = 2
+        }
+
+        // Register Subclasses
+        PendingUser.registerSubclass()
+        Advert.registerSubclass()
+        FeedbackQuestion.registerSubclass()
+        FeedbackType.registerSubclass()
+        Location.registerSubclass()
+        UserSeenAdvert.registerSubclass()
+        UserFeedback.registerSubclass()
+        UserCashout.registerSubclass()
+
+        Parse.initialize(with: configuration)
+    }
+}
+
+extension AppDelegate {
+    func configAdmob() {
+        GADMobileAds.configure(withApplicationID: "ca-app-pub-6220129917785469~1943942885")
+    }
+}
--- Jifflr-iOS/Supporting Files/Constants.swift
+++ Jifflr-iOS/Supporting Files/Constants.swift
@@ -7,8 +7,32 @@
 //
 
 struct Constants {
+
+    static let currentEnvironment: Environment = {
+        if let configuration = Bundle.main.object(forInfoDictionaryKey: "Configuration") as? String {
+            if configuration.lowercased().range(of: "staging") != nil {
+                return Environment.staging
+            }
+        }
+        return Environment.production
+    }()
+
     struct Notifications {
         static let locationFound = Notification.Name(rawValue: "locationFound")
         static let locationPermissionsChanged = Notification.Name(rawValue: "locationPermissionsChanged")
+    }
+}
+
+enum Environment: String {
+    case staging
+    case production
+
+    var appURL: String {
+        switch self {
+        case .staging:
+            return "https://adm8.thecore.thedistance.co.uk/parse"
+        case .production:
+            return "https://adm8.thedistance.co.uk/parse"
+        }
     }
 }
