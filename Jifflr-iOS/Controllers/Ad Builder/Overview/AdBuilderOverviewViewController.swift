3c5b1e22e1f8d581dc2d21d5fb8741a905279f2d
James Shaw <james.shaw@thedistance.co.uk>
2018-04-11 10:59:15 +0100

    JIF-99 - Started building out UI for ad builder overview.

--- Jifflr-iOS/Controllers/Ad Builder/Overview/AdBuilderOverviewViewController.swift
+++ Jifflr-iOS/Controllers/Ad Builder/Overview/AdBuilderOverviewViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class AdBuilderOverviewViewController: BaseViewController {
     
+    @IBOutlet weak var segmentedControl: JifflrSegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableViewHeaderView: UIView!
+    
     class func instantiateFromStoryboard() -> AdBuilderOverviewViewController {
         let storyboard = UIStoryboard(name: "AdBuilderOverview", bundle: nil)
         return storyboard.instantiateViewController(withIdentifier: "AdBuilderOverviewViewController") as! AdBuilderOverviewViewController
@@ -28,10 +32,19 @@ class AdBuilderOverviewViewController: BaseViewController {
         self.setBackgroundImage(image: UIImage(named: "MainBackground"))
         self.navigationController?.isNavigationBarHidden = false
         self.navigationItem.setHidesBackButton(false, animated: false)
+        
+        self.segmentedControl.highlightedColor = UIColor.mainLightBlue
+        self.segmentedControl.delegate = self
+        self.tableView.estimatedRowHeight = 70.0
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
     }
     
     func setupLocalization() {
         self.title = "adBuilderOverview.navigation.title".localized()
+        self.segmentedControl.setButton1Title(text: "adBuilderOverview.segmentedControlButton1.title".localized())
+        self.segmentedControl.setButton2Title(text: "adBuilderOverview.segmentedControlButton2.title".localized())
     }
     
     func updateNavigationStack() {
@@ -42,5 +55,88 @@ class AdBuilderOverviewViewController: BaseViewController {
                 return
             }
         }
+    }
+}
+
+extension AdBuilderOverviewViewController: JifflrSegmentedControlDelegate {
+    func valueChanged() {
+        if self.segmentedControl.selectedSegmentIndex == 0 {
+            self.tableView.contentOffset.y = 0.0
+            self.tableViewHeaderView.frame.size.height = 240.0
+        } else {
+            self.tableView.contentOffset.y = 180.0
+            self.tableViewHeaderView.frame.size.height = 260.0
+        }
+        
+        self.tableView.reloadData()
+    }
+}
+
+extension AdBuilderOverviewViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if self.segmentedControl.selectedSegmentIndex == 0 {
+            return 5
+        }
+        
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if self.segmentedControl.selectedSegmentIndex == 0 {
+            if indexPath.row == 0 {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "ActiveAdsCell") as! ActiveAdsCell
+                cell.nameLabel.text = "adBuilderOverview.totalCampaigns.text".localized()
+                cell.sizeLabel.text = "\(3)"
+                cell.accessoryType = .none
+                cell.selectionStyle = .none
+                return cell
+                
+            } else {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "AdvertCell") as! AdvertCell
+                cell.accessoryType = .none
+                cell.selectionStyle = .none
+                cell.nameLabel.text = "Red Lion Pub Quiz"
+                cell.dateLabel.text = "22 June 2017"
+                cell.idLabel.text = "C# 3"
+                cell.statusLabel.text = "adBuilderOverview.status.text".localized()
+                cell.statusImageView.backgroundColor = UIColor.mainGreen
+                cell.statusImageView.layer.cornerRadius = 7.0
+                cell.statusImageView.layer.masksToBounds = true
+                
+                return cell
+            }
+        } else {
+            if indexPath.row == 0 {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "ActiveAdsCell") as! ActiveAdsCell
+                cell.nameLabel.text = "adBuilderOverview.myActiveAds.text".localized()
+                cell.sizeLabel.text = "\(10)"
+                cell.accessoryType = .none
+                cell.selectionStyle = .none
+                return cell
+                
+            } else {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "AdvertCell") as! AdvertCell
+                cell.accessoryType = .none
+                cell.selectionStyle = .none
+                cell.nameLabel.text = "Early bird"
+                cell.dateLabel.text = "4 August 2017"
+                cell.idLabel.text = "NBIDFNID"
+                cell.statusLabel.text = "adBuilderOverview.status.text".localized()
+                cell.statusImageView.backgroundColor = UIColor.mainGreen
+                cell.statusImageView.layer.cornerRadius = 7.0
+                cell.statusImageView.layer.masksToBounds = true
+                
+                return cell
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
     }
 }
