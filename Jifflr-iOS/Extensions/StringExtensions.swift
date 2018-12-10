f0922e2fd799aeb69c7a349798b93ff075f48c47
James Shaw <james.shaw@thedistance.co.uk>
2018-12-10 12:13:11 +0000

    [JIF-521] Fixed email validation on registration.

--- Jifflr-iOS/Extensions/StringExtensions.swift
+++ Jifflr-iOS/Extensions/StringExtensions.swift
@@ -10,8 +10,9 @@ import Foundation
 
 extension String {
     func isEmail() -> Bool {
-        let regex = try! NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", options: .caseInsensitive)
-        return regex.firstMatch(in: self, options: [], range: NSRange(location: 0, length: count)) != nil
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
     }
     
     func containsNumbers() -> Bool {
